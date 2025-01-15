6a307e349a7d9affb9fb3e60105efe2ee88079ca
Maxfojtik <maxfojtik@bex.net>
2025-01-15 00:04:51 -0500

    Getting started on the landing page (index)

--- ORMFront/src/pages/index.vue
+++ ORMFront/src/pages/index.vue
@@ -1,5 +1,39 @@
 <template>
-
+    <v-card variant="tonal" class="mb-8">
+        <v-card-title>
+            We are currently: <span style="color: red">CLOSED</span> for the season, reopening May 4 2025
+        </v-card-title>
+    </v-card>
+    <v-row>
+        <v-col>
+            Welcome to the Ohio Railway Museum! <br><br>
+            We are a museum located in Worthington, Ohio, that focuses on the railway and streetcar history of Ohio.<br><br>
+        </v-col>
+        <v-col cols="8">
+            <v-carousel cycle hide-delimiters show-arrows="hover">
+            <v-carousel-item
+                src="../assets/carousel/44967337_10155572891881607_1766772425418080256_n.jpg"
+                cover
+            ></v-carousel-item>
+            <v-carousel-item
+                src="../assets/carousel/35883020_10155319779091607_1744041813515173888_n.jpg"
+                cover
+            ></v-carousel-item>
+            <v-carousel-item
+                src="../assets/carousel/31905644_10155221094451607_7797266680664555520_n.jpg"
+                cover
+            ></v-carousel-item>
+            <v-carousel-item
+                src="../assets/carousel/468353391_10160240052706607_4057460574763797311_n.jpg"
+                cover
+            ></v-carousel-item>
+            <v-carousel-item
+                src="../assets/carousel/473360060_2707139446161257_3075118943565293160_n.jpg"
+                cover
+            ></v-carousel-item>
+            </v-carousel>
+        </v-col>
+    </v-row>
 </template>
 
 <style></style>
